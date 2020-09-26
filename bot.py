e2ef131be78faa071851eff356a976dc5caceb48
Holly <h.holly3003@gmail.com>
2020-09-26 13:44:16 +1000

    create get_update instance method: getting updates of data that is sent to the bot

--- bot.py
+++ bot.py
@@ -13,3 +13,12 @@ class TelegramChatbot:
         parser = configparser.ConfigParser()
         parser.read(config)
     
+    #getting all updates or messages that is sent to the bot
+    def get_updates(self, offset = None):
+        #use the getUpdates method to get all data and set a timeout parameter of 60s
+        url = self.base + "getUpdates?timeout=60"
+        if offset:
+            url = url + f"&offset={offset + 1}"
+        response = requests.get(url)
+        return json.loads(response.content)
+    
