a2b0191ec3994b898d07e57718469532c5243f24
Holly <h.holly3003@gmail.com>
2020-09-26 14:05:26 +1000

    Create send_message instance method: bot sending response to the user

--- bot.py
+++ bot.py
@@ -17,8 +17,17 @@ class TelegramChatbot:
     def get_updates(self, offset = None):
         #use the getUpdates method to get all data and set a timeout parameter of 60s
         url = self.base + "getUpdates?timeout=60"
+        # if the offset parameter is provided, will add the offset parameter in the url
         if offset:
             url = url + f"&offset={offset + 1}"
         response = requests.get(url)
         return json.loads(response.content)
     
+    #Sending message or response from bot to the specified user
+    def send_message(self, message, chat_id):
+        #use the sendMessage method and specify the reciever and text to send
+        url = self.base + f"sendMessage?chat_id={chat_id}&text={message}"
+        # Only if there is message to send, it will sends a post request
+        if message is not None:
+            response = requests.post(url)
+
