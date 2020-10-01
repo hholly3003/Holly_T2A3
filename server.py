0d7b56e3c04896a2c2d3ea683e88770ccf41b531
Holly <h.holly3003@gmail.com>
2020-10-01 14:12:15 +1000

    Handling incoming message using command_run

--- server.py
+++ server.py
@@ -8,6 +8,35 @@ def bot_response(message):
         reply = f"I have stored message : '{message}' into log file"
     return reply
 
+def run_command(sender, first_name, last_name, text, file_id, update_id, file_size, date):
+    global EXIT_MODE
+    if text == "/help":
+        chatbot.send_message(f"No help today. Sorry, {first_name}", sender)
+        
+    elif text == '/start':
+        chatbot.send_message(f"Hello {first_name}! Just send me a text or picture file", sender)
+        
+    elif text == '/exit':
+        chatbot.send_message("Terminating the bot", sender)
+        EXIT_MODE = True
+
+    elif file_id is not None:
+        chatbot.display_incoming_message(date, first_name, last_name, text, file_size)
+        file_details = chatbot.get_file_details(file_id)
+        # Check photo's file size is not more than 20MB
+        if check_photo(file_details):
+            file_path, file_name = get_photo_details(file_details)
+            # token = chatbot.read_token_from_config_file("config.cfg")
+            photo_url = f"https://api.telegram.org/file/bot{chatbot.token}/{file_path}"          
+            download_status = download_photo(photo_url,file_name)
+            chatbot.send_message(download_status, sender)
+        else:
+            chatbot.send_message("Unable to download, The file size is exceeding 20MB", sender)
+    else:
+        chatbot.display_incoming_message(date, first_name, last_name, text, file_size)
+        reply = bot_response(text)
+        chatbot.send_message(reply,sender)
+
 update_id = None
 print("Bot is running ...")
 while True:    
