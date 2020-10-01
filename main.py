8618372082e9248040703c93fdb2af3d721d9599
Holly <h.holly3003@gmail.com>
2020-10-01 14:19:42 +1000

    Refactor the code and run application using main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import bot
+from server import check_updates
+import sys
+
+
+chatbot = bot.TelegramChatbot("config.cfg")
+print("Chatbot is running . . . .")   
+update_id = None
+EXIT_MODE = False
+
+while True:
+    try:
+        if not EXIT_MODE:
+            update_id = check_updates(update_id)
+            print(update_id)
+        else:
+            sys.exit()
+    except KeyboardInterrupt:
+        print('Interrupt by user..')
+        break
--- server.py
+++ server.py
@@ -37,26 +37,3 @@ def run_command(sender, first_name, last_name, text, file_id, update_id, file_si
         chatbot.display_incoming_message(date, first_name, last_name, text, file_size)
         reply = bot_response(text)
         chatbot.send_message(reply,sender)
-
-update_id = None
-print("Bot is running ...")
-while True:    
-    # Instantiate the telegramchatbot
-    chatbot = bot.TelegramChatbot("config.cfg")
-    # Check for any update or input given to the bots
-    updates = chatbot.get_updates(offset = update_id)
-    updates = updates[0]
-    if updates:
-        
-        update_id = updates[5]
-        print(update_id)
-        if updates[3] != None:
-            reply = bot_response(updates[3])
-            chatbot.send_message(reply, updates[0])
-        if updates[4] != None:
-            file_details = chatbot.get_file_details(updates[4])
-            file_path = file_details.get("file_path", "")
-            file_name = file_path.split("/")[1]
-            # download_status = chatbot.download_photo(file_path, file_name)
-            # chatbot.send_message(download_status,updates[0])
-                
